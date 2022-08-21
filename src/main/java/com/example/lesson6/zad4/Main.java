dd4268304bebaaa7074681d784e2a08923f01fa6
Maciek <maciejbella@op.pl>
2022-08-21 14:09:18 +0200

    Dodano zadanie 4

---
+++ src/main/java/com/example/lesson6/zad4/Main.java
@@ -0,0 +1,10 @@
+package com.example.lesson6.zad4;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String result = DaysOfTheWeek.getDaysOfTheWeek(9);
+        System.out.println(result);
+    }
+}
