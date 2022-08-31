a3b25a3c6e8ad6c24519e356cb64c78735833780
Maciek <maciejbella@op.pl>
2022-08-31 16:59:22 +0200

    Poprawiono zadanie 1

--- src/main/java/com/example/lesson6/zad1/Calculator.java
+++ src/main/java/com/example/lesson6/zad1/Calculator.java
@@ -2,19 +2,19 @@ package com.example.lesson6.zad1;
 
 public class Calculator {
 
-    public static int sum(int numbers1, int numbers2) {
-        return numbers1 + numbers2;
+    public static int sum(int number1, int number2) {
+        return number1 + number2;
     }
 
-    public static int subtract(int numbers1, int numbers2) {
-        return numbers1 - numbers2;
+    public static int subtract(int number1, int number2) {
+        return number1 - number2;
     }
 
-    public static int multiply(int numbers1, int numbers2) {
-        return numbers1 * numbers2;
+    public static int multiply(int number1, int number2) {
+        return number1 * number2;
     }
 
-    public static int division(int numbers1, int numbers2) {
-        return numbers1 / numbers2;
+    public static int divide(int number1, int number2) {
+        return number1 / number2;
     }
 }
